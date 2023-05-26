5d9c63bf5f15298c3c9659abb491f5b594125e4d
rayanemelo <raymeloc@gmail.com>
2023-05-26 12:09:06 -0300

    first commit

---
+++ react/ProductInstallments.tsx
@@ -0,0 +1,3 @@
+import {ProductInstallments} from './components/ProductInstallments'
+
+export default ProductInstallments
---
+++ react/components/ProductInstallments.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export function ProductInstallments() {
+  return <h1>Product Installments</h1>
+}
